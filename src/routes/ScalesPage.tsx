0ee8f52a516b7b8dbe7efa667da0886914abe849
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2023-12-09 00:49:09 +0100

    chore: add not supported message for mobile

--- src/routes/ScalesPage.tsx
+++ src/routes/ScalesPage.tsx
@@ -1,23 +1,29 @@
-import Settings from "@/components/modules/settings/Settings";
 import ScaleDetails from "@/components/modules/scale-details/ScaleDetails";
 import MenuTop from "@/components/modules/navigation/MenuTop";
-import Library from "@/components/modules/library/Library";
+import TabsScales from "@/components/ui/TabsScales";
+import { useMediaQuery } from "@mantine/hooks";
+import MobileNotSupported from "@/components/layout/MobileNotSupported";
 
 export default function ScalesPage() {
+  const matches = useMediaQuery("(max-width: 768px)");
+
+  if (matches) {
+    return <MobileNotSupported />;
+  }
+
   return (
     <>
-    <MenuTop />
-    <main>
-      <div className="container flex flex-col items-center justify-center py-12">
-        <div className="flex flex-row">
-          <ScaleDetails />
+      <MenuTop />
+      <main>
+        <div className="container flex flex-col items-center justify-center py-12">
+          <div className="flex flex-row">
+            <ScaleDetails />
+          </div>
         </div>
-      </div>
-      <section className="py-20">
-        <Library variant="scales" />
-      </section>
-      <Settings />
-    </main>
-  </>
+        <section className="py-20">
+          <TabsScales />
+        </section>
+      </main>
+    </>
   );
 }
