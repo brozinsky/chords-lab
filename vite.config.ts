84d07783e497d64bab80f155104b02ccdc9baff5
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2023-12-13 23:14:53 +0100

    ci: adjust config path

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from 'path';
 
-const entryFile = "src/main.jsx";
+const entryFile = "main.tsx";
 
 // https://vitejs.dev/config/
 export default defineConfig({
