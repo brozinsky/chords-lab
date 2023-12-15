fa4a316b6f81e3afc9c888b84e0cd9fe8a66e72d
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2023-12-15 01:13:37 +0100

    ci: adjust vite build config

--- vite.config.ts
+++ vite.config.ts
@@ -2,18 +2,16 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import * as path from 'path';
 
-const entryFile = "main.tsx";
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/chords-lab/",
+  // base: "/chords-lab/",
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
   },
   build: {
     rollupOptions: {
-      input: path.resolve(__dirname, `src/${entryFile}`),
+      input: ['src/main.tsx', './index.html']
     },
   },
 });
