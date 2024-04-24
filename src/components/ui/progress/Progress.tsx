fd304a4dedcf32aeac89c52beee1603fe960bba2
Mateusz Brzeziński <mateusz0brzezinski@gmail.com>
2024-04-24 23:21:38 +0200

    feat(ui): add progress component

---
+++ src/components/ui/progress/Progress.tsx
@@ -0,0 +1,31 @@
+import * as React from "react";
+import * as ProgressPrimitive from "@radix-ui/react-progress";
+import { cn } from "@/lib/utils";
+
+interface IProgressProps
+  extends React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root> {
+  indicatorColor?: string;
+}
+
+const Progress = React.forwardRef<
+  React.ElementRef<typeof ProgressPrimitive.Root>,
+  IProgressProps
+>(({ className, indicatorColor = "bg-primary-500", value, ...props }, ref) => (
+  <ProgressPrimitive.Root
+    ref={ref}
+    className={cn(
+      "relative h-2 w-full overflow-hidden rounded-full bg-neutral-400 border-none",
+      className
+    )}
+    {...props}
+  >
+    <ProgressPrimitive.Indicator
+      className={cn(indicatorColor, "h-full w-full flex-1 transition-all")}
+      style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+    />
+  </ProgressPrimitive.Root>
+));
+
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export default Progress;
